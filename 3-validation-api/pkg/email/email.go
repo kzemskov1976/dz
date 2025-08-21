bc22bf0f4928764c2b6cc7186707eb2d11235851
k.zemskov <kirill@zemskoff.su>
2025-08-22 00:28:10 +0300

    fix4

--- 3-validation-api/pkg/email/email.go
+++ 3-validation-api/pkg/email/email.go
@@ -12,7 +12,7 @@ func SendEmail(from, pwd, smtpaddr, to, hash string) error {
 	e.From = from
 	e.To = []string{to}
 	e.Subject = "Email validation"
-	e.HTML = fmt.Appendf(nil, "<a>http://localhost:8081/verify/%s</a>", hash)
+	e.HTML = fmt.Appendf(nil, "<a href\"http://localhost:8081/verify/%s\">Подтвердить email</a>", hash)
 	err := e.Send("smtp.gmail.com:587", smtp.PlainAuth("", from, pwd, smtpaddr))
 	return err
 }
